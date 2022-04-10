581bb0d9fce07356eebad07848e6d948f2de190d
dentremontcatwit <dentremontc@wit.edu>
2022-04-10 01:13:57 -0400

    Added view functionality

--- applications.js
+++ applications.js
@@ -1,5 +1,21 @@
 const cardList = document.querySelector("#cards");
 
+//form modal
+const newAppButton = document.querySelector("#addapplication");
+const testViewButton = document.querySelector("#testview");
+const modalBg = document.querySelector("#formModalBg");
+const modal = document.querySelector("#formModal");
+
+//form
+const applicationForm = document.querySelector("#applicationform");
+const formSubmitButton = document.querySelector("#formsubmit");
+const invalidInput = document.querySelector("#invalidinput");
+
+//view modal
+const viewModal = document.querySelector("#viewModal");
+const viewModalBg = document.querySelector("#viewModalBg");
+const viewModalContent = document.querySelector("#viewModalContent");
+
 window.onload = function WindowLoad(event) {
   //Load and display cookies
   var cookies = document.cookie.split(";");
@@ -22,7 +38,7 @@ window.onload = function WindowLoad(event) {
     //Card div
     var newCard = document.createElement("div");
     newCard.classList.add("card");
-    var cardID = currentDisplay[0].replace(/\s/g, "");
+    var cardID = "" + current[0];
     newCard.setAttribute("id", cardID);
 
     //Card content
@@ -47,7 +63,115 @@ window.onload = function WindowLoad(event) {
     viewLink.classList.add("card-footer-item");
     var viewLinkButton = document.createElement("a");
     viewLinkButton.addEventListener("click", () => {
-      console.log("view " + currentDisplay[0]);
+      //Title
+      var modalTitle = document.createElement("h3");
+      modalTitle.classList.add("title");
+      modalTitle.classList.add("has-text-weight-bold");
+      modalTitle.classList.add("mb-6");
+      modalTitle.appendChild(
+        document.createTextNode(
+          "" + currentDisplay[1] + " @ " + currentDisplay[0]
+        )
+      );
+
+      //Company Name
+      var viewCompanyNameTitle = document.createElement("h5");
+      viewCompanyNameTitle.classList.add("subtitle");
+      viewCompanyNameTitle.classList.add("is-4");
+      viewCompanyNameTitle.classList.add("mb-1");
+      viewCompanyNameTitle.classList.add("has-text-weight-bold");
+      viewCompanyNameTitle.appendChild(document.createTextNode("Company"));
+      var viewCompanyName = document.createElement("p");
+      viewCompanyName.classList.add("mb-3");
+      viewCompanyName.appendChild(document.createTextNode(currentDisplay[0]));
+
+      //Position
+      var viewPositionTitle = document.createElement("h5");
+      viewPositionTitle.classList.add("subtitle");
+      viewPositionTitle.classList.add("is-4");
+      viewPositionTitle.classList.add("mb-1");
+      viewPositionTitle.classList.add("has-text-weight-bold");
+      viewPositionTitle.appendChild(document.createTextNode("Position"));
+      var viewPosition = document.createElement("p");
+      viewPosition.classList.add("mb-3");
+      viewPosition.appendChild(document.createTextNode(currentDisplay[1]));
+
+      //Position Type
+      var viewPositionTypeTitle = document.createElement("h5");
+      viewPositionTypeTitle.classList.add("subtitle");
+      viewPositionTypeTitle.classList.add("is-4");
+      viewPositionTypeTitle.classList.add("mb-1");
+      viewPositionTypeTitle.classList.add("has-text-weight-bold");
+      viewPositionTypeTitle.appendChild(
+        document.createTextNode("Position Type")
+      );
+      var viewPositionType = document.createElement("p");
+      viewPositionType.classList.add("mb-3");
+      viewPositionType.appendChild(document.createTextNode(currentDisplay[2]));
+
+      //Status
+      var viewStatusTitle = document.createElement("h5");
+      viewStatusTitle.classList.add("subtitle");
+      viewStatusTitle.classList.add("is-4");
+      viewStatusTitle.classList.add("mb-1");
+      viewStatusTitle.classList.add("has-text-weight-bold");
+      viewStatusTitle.appendChild(
+        document.createTextNode("Application Status")
+      );
+      var viewStatus = document.createElement("p");
+      viewStatus.classList.add("mb-3");
+      viewStatus.appendChild(document.createTextNode(currentDisplay[3]));
+
+      //Date Applied
+      var viewDateAppliedTitle = document.createElement("h5");
+      viewDateAppliedTitle.classList.add("subtitle");
+      viewDateAppliedTitle.classList.add("is-4");
+      viewDateAppliedTitle.classList.add("mb-1");
+      viewDateAppliedTitle.classList.add("has-text-weight-bold");
+      viewDateAppliedTitle.appendChild(document.createTextNode("Date Applied"));
+      var viewDateApplied = document.createElement("p");
+      viewDateApplied.classList.add("mb-3");
+      if (currentDisplay[4].length == 0) {
+        viewDateApplied.appendChild(document.createTextNode("N/A"));
+      } else {
+        viewDateApplied.appendChild(document.createTextNode(currentDisplay[4]));
+      }
+
+      //Notes
+      var viewNotesTitle = document.createElement("h5");
+      viewNotesTitle.classList.add("subtitle");
+      viewNotesTitle.classList.add("is-4");
+      viewNotesTitle.classList.add("mb-1");
+      viewNotesTitle.classList.add("has-text-weight-bold");
+      viewNotesTitle.appendChild(document.createTextNode("Notes"));
+      var viewNotes = document.createElement("p");
+      viewNotes.classList.add("mb-3");
+      if (currentDisplay[5].length == 0) {
+        viewNotes.appendChild(document.createTextNode("N/A"));
+      } else {
+        viewNotes.appendChild(document.createTextNode(currentDisplay[5]));
+      }
+
+      viewModalContent.appendChild(modalTitle);
+      viewModalContent.appendChild(viewCompanyNameTitle);
+      viewModalContent.appendChild(viewCompanyName);
+      viewModalContent.appendChild(viewPositionTitle);
+      viewModalContent.appendChild(viewPosition);
+      viewModalContent.appendChild(viewPositionTypeTitle);
+      viewModalContent.appendChild(viewPositionType);
+      viewModalContent.appendChild(viewStatusTitle);
+      viewModalContent.appendChild(viewStatus);
+      viewModalContent.appendChild(viewDateAppliedTitle);
+      viewModalContent.appendChild(viewDateApplied);
+      viewModalContent.appendChild(viewNotesTitle);
+      viewModalContent.appendChild(viewNotes);
+      viewModal.classList.add("is-active");
+    });
+    viewModalBg.addEventListener("click", () => {
+      while (viewModalContent.firstChild) {
+        viewModalContent.removeChild(viewModalContent.firstChild);
+      }
+      viewModal.classList.remove("is-active");
     });
     viewLinkButton.classList.add("has-text-gray");
     viewLinkText = document.createTextNode("View");
@@ -58,7 +182,7 @@ window.onload = function WindowLoad(event) {
     deleteLink.classList.add("card-footer-item");
     var deleteLinkButton = document.createElement("a");
     deleteLinkButton.addEventListener("click", () => {
-      var id = currentDisplay[0].replace(/\s/g, "");
+      var id = "" + current[0];
       document.cookie =
         "" + id + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     });
@@ -76,16 +200,6 @@ window.onload = function WindowLoad(event) {
     cardList.appendChild(newCard);
   }
 };
-
-//modal
-const newAppButton = document.querySelector("#addapplication");
-const modalBg = document.querySelector(".modal-background");
-const modal = document.querySelector(".modal");
-
-//form
-const applicationForm = document.querySelector("#applicationform");
-const formSubmitButton = document.querySelector("#formsubmit");
-const invalidInput = document.querySelector("#invalidinput");
 
 newAppButton.addEventListener("click", () => {
   modal.classList.add("is-active");
@@ -111,7 +225,8 @@ formSubmitButton.addEventListener("click", () => {
     for (var i = 0, element; (element = elements[i++]); ) {
       formElements.push(element.value);
     }
-    var id = elements[0].value.replace(/\s/g, "");
+    var idNum = Math.floor(Math.random() * 90000) + 10000;
+    var id = "" + elements[0].value.replace(/\s/g, "") + idNum;
     const finalElements =
       "" +
       id +
